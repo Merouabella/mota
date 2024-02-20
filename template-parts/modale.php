0b8e72392eaefce630b6d0f0633d1f4a2cb19bb5
Merouabella <131451877+Merouabella@users.noreply.github.com>
2024-02-20 15:01:44 +0100

    template-parts

---
+++ template-parts/modale.php
@@ -0,0 +1,9 @@
+<div class="popup-overlay">
+	<div class="popup-contact">
+	   
+        <img src="<?php echo get_template_directory_uri() . '/assets/images/Contact_header.png'; ?>" alt="contact">
+		<?php
+		echo do_shortcode('[contact-form-7 id="0fa0724" title="Contact"]');
+		?>
+	</div>
+</div>
