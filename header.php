91937b4bc65df25ff1f6a22e5a46e40e5ac5c19f
Merouabella <131451877+Merouabella@users.noreply.github.com>
2024-03-20 12:05:17 +0100

    etape 1

--- header.php
+++ header.php
@@ -8,8 +8,6 @@
 </head>
 
 <body class="mainContainer">
-<?php wp_body_open(); ?>
-
 	<header class="site-header">
 		<nav id="site-navigation" class="lienNav" role="navigation">
 			<div class="logo">
@@ -39,3 +37,13 @@
 		</nav>
 
 	</header>
+	
+	<main class="site-main">
+		<?php
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content', get_post_format() );
+			}
+		} else {
+			
